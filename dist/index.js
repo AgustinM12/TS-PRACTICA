0a643485d09b315b0a308c56d3eae7a76b0c48c7
Agustin.M12 <agustinmazza12345@gmail.com>
2024-07-26 12:45:53 -0300

    FEAT: se agregaron nuevas prubeas de TS

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,32 @@
 "use strict";
 let mensaje = "Hola mundo";
+const animales = ["perro", "gato", "goat"];
+const nums = [1, 2, 3, 4, 5];
+function funcion(retorno) {
+    return retorno;
+}
+let tupla = [1, 'hola'];
+var Talla;
+(function (Talla) {
+    Talla[Talla["Chica"] = 2] = "Chica";
+    Talla[Talla["Mediana"] = 3] = "Mediana";
+    Talla[Talla["Grande"] = 4] = "Grande";
+    Talla[Talla["ExtraGrande"] = 5] = "ExtraGrande";
+})(Talla || (Talla = {}));
+const estado = 2;
+const objeto = {
+    id: 1,
+    nombre: 'Juan'
+};
+const objetoPrueba = {
+    id: 1,
+    nombre: "Juan",
+    direccion: {
+        numero: 123,
+        calle: "Calle falsa",
+        pais: "Arg"
+    },
+    talla: Talla.Grande
+};
+console.log(objetoPrueba);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,51 @@
 let mensaje: string = "Hola mundo"
+
+const animales: string[] = ["perro", "gato", "goat"]
+
+const nums: number[] = [1, 2, 3, 4, 5]
+
+function funcion(retorno: string) {
+    return retorno
+}
+
+let tupla: [number, string] = [1, 'hola']
+
+enum Talla { Chica = 2, Mediana, Grande, ExtraGrande }
+
+const enum Carga { Idle, Loading, Success, Error }
+
+const estado = Carga.Success
+
+const objeto: { id: number, nombre: string } = {
+    id: 1,
+    nombre: 'Juan'
+}
+
+// DEFINICION DE UN TIPO DE OBJETO, UN MODELO DE COMO SERAN LOS OBJETOS
+type Direccion = {
+    numero: number,
+    calle: string,
+    pais: string
+}
+
+// TIPO DE OBJETO QUE INCLUYE OTRO TIPO (DIRECCION) Y AL ENUM
+type Persona = {
+    readonly id: number,
+    nombre: string,
+    talla: Talla,
+    direccion: Direccion
+}
+
+// DEFINIR UNA INSTACIA DEL OBJETO PERSONA
+const objetoPrueba: Persona = {
+    id: 1,
+    nombre: "Juan",
+    direccion: {
+        numero: 123,
+        calle: "Calle falsa",
+        pais: "Arg"
+    },
+    talla: Talla.Grande
+}
+
+console.log(objetoPrueba);
